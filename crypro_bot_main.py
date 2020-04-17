186caa053fc34d61eac98b576b74cec94a1ef63b
Sergey K <kuacho@gmail.com>
2020-04-17 21:54:44 +0300

    Command Satart, optimize tale
    Added start command handler.

--- crypro_bot_main.py
+++ crypro_bot_main.py
@@ -24,54 +24,65 @@ def get_all_tokens():
     return all_tokens
 
 
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id,
+                     "🕺 Let's check how much ₿ or Ripple is worth right now 📈 Or maybe you want to see the cost of TOP🔟 coins. It`s easy.\n"
+                     "\nJust tap on the commands: \n"
+                     "/top10 - check TOP10 price crypto by Binance\n"
+                     "/btc - check BTC price by Binance\n"
+                     "/xrp - check Ripple price by Binance\n"
+                     "/eth - check Ethereum price by Binance")
+
+
 @bot.message_handler(commands=['btc'])
 def price_btc(message):
     get_all_tokens()
 
     for token in all_tokens:
         if 'BTCUSDT' in token['symbol']:
-            last_price = token['price'] + ' ' + 'usdt'
+            last_price = '1 BTC = ' + ' ' + token['price'] + ' ' + 'usdt'
             bot.send_message(message.chat.id, last_price)
             all_tokens.clear()
 
 
 @bot.message_handler(commands=['xrp'])
-def price_btc(message):
+def price_xrp(message):
     get_all_tokens()
 
     for token in all_tokens:
         if 'XRPUSDT' in token['symbol']:
-            last_price = token['price'] + ' ' + 'usdt'
+            last_price = '1 XRP = ' + ' ' + token['price'] + ' ' + 'usdt'
             bot.send_message(message.chat.id, last_price)
             all_tokens.clear()
 
 
 @bot.message_handler(commands=['eth'])
-def price_btc(message):
+def price_eth(message):
     get_all_tokens()
 
     for token in all_tokens:
         if 'ETHUSDT' in token['symbol']:
-            last_price = token['price'] + ' ' + 'usdt'
+            last_price = '1 ETH = ' + ' ' + token['price'] + ' ' + 'usdt'
             bot.send_message(message.chat.id, last_price)
             all_tokens.clear()
 
 
 @bot.message_handler(commands=['top10'])
-def price_btc(message):
+def price_top10(message):
     get_all_tokens()
-    print(all_tokens[0:11])
 
-    bot.send_message(message.chat.id, f"{all_tokens[0]['symbol'].split('USDT')[0]}  {all_tokens[0]['price']} usdt\n"
-                                      f"{all_tokens[1]['symbol'].split('USDT')[0]}  {all_tokens[1]['price']} usdt\n"
-                                      f"{all_tokens[2]['symbol'].split('USDT')[0]}  {all_tokens[2]['price']} usdt\n"
-                                      f"{all_tokens[3]['symbol'].split('USDT')[0]}  {all_tokens[3]['price']} usdt\n"
-                                      f"{all_tokens[4]['symbol'].split('USDT')[0]}  {all_tokens[4]['price']} usdt\n"
-                                      f"{all_tokens[5]['symbol'].split('USDT')[0]}  {all_tokens[5]['price']} usdt\n"
-                                      f"{all_tokens[6]['symbol'].split('USDT')[0]}  {all_tokens[6]['price']} usdt\n"
-                                      f"{all_tokens[7]['symbol'].split('USDT')[0]}  {all_tokens[7]['price']} usdt\n"
-                                      f"{all_tokens[8]['symbol'].split('USDT')[0]}  {all_tokens[8]['price']} usdt\n"
-                                      f"{all_tokens[9]['symbol'].split('USDT')[0]}  {all_tokens[9]['price']} usdt\n")
+    bot.send_message(message.chat.id, f"1{all_tokens[0]['symbol'].split('USDT')[0]}  = {all_tokens[0]['price']} usdt\n"
+                                      f"1{all_tokens[1]['symbol'].split('USDT')[0]}  = {all_tokens[1]['price']} usdt\n"
+                                      f"1{all_tokens[2]['symbol'].split('USDT')[0]}  = {all_tokens[2]['price']} usdt\n"
+                                      f"1{all_tokens[3]['symbol'].split('USDT')[0]}  = {all_tokens[3]['price']} usdt\n"
+                                      f"1{all_tokens[4]['symbol'].split('USDT')[0]}  = {all_tokens[4]['price']} usdt\n"
+                                      f"1{all_tokens[5]['symbol'].split('USDT')[0]}  = {all_tokens[5]['price']} usdt\n"
+                                      f"1{all_tokens[6]['symbol'].split('USDT')[0]}  = {all_tokens[6]['price']} usdt\n"
+                                      f"1{all_tokens[7]['symbol'].split('USDT')[0]}  = {all_tokens[7]['price']} usdt\n"
+                                      f"1{all_tokens[8]['symbol'].split('USDT')[0]}  = {all_tokens[8]['price']} usdt\n"
+                                      f"1{all_tokens[9]['symbol'].split('USDT')[0]}  = {all_tokens[9]['price']} usdt\n"
+                                      f"\nCrypto to the moon 🚀")
 
 
 if __name__ == '__main__':
